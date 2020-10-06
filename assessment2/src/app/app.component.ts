0f4b8297e247f5dc777697372931f69c537f02fe
Imran01000 <imshaikh01000@gmail.com>
2020-10-07 00:18:58 +0530

    ADDED:Added typescript file

---
+++ assessment2/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'AboutMe',
+      url: '/aboutme',
+      icon: 'person'
+    },
+    {
+      title: 'Skills&experience',
+      url: '/home/skills-experience',
+      icon: 'folder'
+    },
+    {
+      title: 'Resume',
+      url: '/resume',
+      icon: 'document-text'
+    },
+    {
+      title: 'MyWork',
+      url: '/home/my-work',
+      icon: 'briefcase'
+    }
+  ];
+  
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('home/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
+}
